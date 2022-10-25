a7cb6539a6c6eb758a16a87129543db0cc2231cf
Francisco Javier Caballe <francaballe@gmail.com>
2022-10-25 11:31:15 -0300

    cambie el include del findall, mas prolijo

--- PI-Countries-main/api/src/controllers/getActivities.js
+++ PI-Countries-main/api/src/controllers/getActivities.js
@@ -1,13 +1,15 @@
 //Consumo mi modelo de BBDD. Solo necesito Activity en este caso
-const { Activity } = require('../db');
+const { Activity,Country } = require('../db');
 
 const getActivities = async function () {
     
   //Uso tanto await aca como en la ruta solo a los efectos de poder hacer un console.log aca...
   try{
-    const resp = await Activity.findAll()
+    
+    const resp = await Activity.findAll({include:Country})    
+
+    /* const resp = await Activity.findAll()
     for (let i=0;i<resp.length;i++){
-      //console.log(resp) si consologueo resp, veo que "dataValues" es un objeto dentro de, en este caso, mi modelo "Activity"
       resp[i].dataValues.countries = [];  //con estoy estoy creando una nueva property, es como un virtual field
     }
 
@@ -15,14 +17,13 @@ const getActivities = async function () {
       let paisesDelaActividad = await resp[i].getCountries();
       let sizeArrayDePaisesDeUnaActividad = paisesDelaActividad.length;//HERMOSO el nombre de mi variable jajajaj
       for (let j=0;j<sizeArrayDePaisesDeUnaActividad;j++){
-        //console.log(paisesDelaActividad[j].Id)
         resp[i].dataValues.countries.push(
           {
             Id: paisesDelaActividad[j].Id,
             name: paisesDelaActividad[j].name
           }
           )}  
-    }
+    } */
 
     return resp;
   }catch{
